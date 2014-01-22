da3981e55f9b28a10898ccd3e4c4b5efb6ebecb2
Vladimir Vedernikov <vedernikov@asymbix.com>
2014-01-22 17:30:18 +0400

    Initial commit

---
+++ app/src/main/java/ru/rarus/app/xml/Parser.java
@@ -0,0 +1,17 @@
+package ru.rarus.app.xml;
+
+
+
+/**
+ * Class: Parser
+ *
+ * @author <a href="mailto:vedernikov@omnicomm.ru">Vladimir Vedernikov</a>
+ */
+public class Parser {
+    private String xml;
+
+    public Request parse(String xml) {
+        Request request = new Request();
+        return request;
+    }
+}
